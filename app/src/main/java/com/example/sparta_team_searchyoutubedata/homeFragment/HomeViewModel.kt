ee3f8ebc9577f014484593b4febbf8fc0e896f3b
JYeyun <117142316+JYeyun@users.noreply.github.com>
2024-05-17 16:03:27 +0900

    add
    add 홈 fragment 기능

---
+++ app/src/main/java/com/example/sparta_team_searchyoutubedata/homeFragment/HomeViewModel.kt
@@ -0,0 +1,90 @@
+package com.example.sparta_team_searchyoutubedata.homeFragment
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.sparta_team_searchyoutubedata.network.client.RetrofitClient
+import com.example.sparta_team_searchyoutubedata.network.data.repository.YoutubeDataRepository
+import com.example.sparta_team_searchyoutubedata.network.data.repository.YoutubeDataRepositoryImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val repository:YoutubeDataRepository
+) : ViewModel(){
+    private val _uiState = MutableStateFlow(HomeUiState.init())
+    val uiState:StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+    fun loadPopular() = viewModelScope.launch {
+        val itemList = repository.getVideos("snippet", "mostPopular", 5U, "0").items?.map{
+            HomeItemModel(
+                title = it.snippet?.title ?: "",
+                thumbnails = it.snippet?.thumbnails?.default?.url ?: "",
+                description = it.snippet?.description ?: "",
+                channelID = it.snippet?.channelId ?: ""
+            )
+        }
+        itemList?.let { list ->
+            _uiState.update { prev ->
+                prev.copy(
+                    list = list
+                )
+            }
+        }
+    }
+    fun loadCategory(id : String) = viewModelScope.launch {
+        val categoryList = repository.getVideos("snippet", "mostPopular", 5U, id).items?.map{
+            HomeItemModel(
+                title = it.snippet?.title ?: "",
+                thumbnails = it.snippet?.thumbnails?.default?.url ?: "",
+                description = it.snippet?.description ?: "",
+                channelID = it.snippet?.channelId ?: ""
+            )
+        }
+        val channeIdlList =  repository.getVideos("snippet", "mostPopular", 5U, id).items?.map{
+            it.snippet?.channelId ?: ""
+        }
+        if (channeIdlList != null) {
+            loadChannel(channeIdlList)
+        }
+        categoryList?.let { list ->
+            _uiState.update { prev ->
+                prev.copy(
+                    listC = list
+                )
+            }
+        }
+    }
+    fun loadChannel(id: List<String>) = viewModelScope.launch {
+        var channelList : MutableList<HomeItemModel> = mutableListOf()
+        for(i in 0 .. 4){
+            repository.getChannel("snippet", 5U, id[i]).items?.forEach {
+                channelList.add(HomeItemModel(
+                    title = it.snippet?.title ?: "",
+                    thumbnails = it.snippet?.thumbnails?.default?.url ?: "",
+                    description = it.snippet?.description ?: "",
+                    channelID = id[i]
+                ))
+            }
+
+        }
+        channelList?.let { list ->
+            _uiState.update { prev ->
+                prev.copy(
+                    listChannel = list
+                )
+            }
+        }
+    }
+}
+class HomeViewModelFactory : ViewModelProvider.Factory{
+    private val repository = YoutubeDataRepositoryImpl(RetrofitClient.youtubeDataRemote)
+
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>
+    ) : T = HomeViewModel(repository) as T
+}
